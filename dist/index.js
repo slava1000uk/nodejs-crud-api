33281cf7dd7243efd62b12c9d366a7b51d44e122
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-03 12:46:44 +0200

    feat: init simple server index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
-"use strict";
-const message = 'Hello, typescript!';
-console.log(message);
-//# sourceMappingURL=index.js.map
+import http from "http";
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ data: true }));
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const message: string = 'Hello, typescript!';
-console.log(message);
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ data: true }));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
