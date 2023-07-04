fa3ea831c4d00ece7811c9663100820317fca08a
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-04 18:43:06 +0200

    feat: add get method handler

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,54 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const node_http_1 = __importDefault(require("node:http"));
-const server = node_http_1.default.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ data: 'Viacheslav Tyshchuk' }));
+const node_http_1 = require("node:http");
+;
+;
+let users = [];
+const getAllUsers = () => users;
+const URL_BEFORE_ID_REGEXP = /^\/api\/users\/$/;
+const getIdFromURL = (url) => {
+    let id = url.replace(URL_BEFORE_ID_REGEXP, '');
+    if (id.endsWith('/')) {
+        id = id.slice(0, id.length - 1);
+    }
+    return id;
+};
+const getUserById = (id) => {
+    const userById = users.find(user => user.id === id);
+    return userById;
+};
+const server = (0, node_http_1.createServer)((request, response) => {
+    const isUrlEndsWithUsers = (request.url === '/api/users') || (request.url === '/api/users/');
+    let output;
+    response.setHeader("Content-Type", "application/json");
+    // response.statusCode = 200;
+    // response.end(JSON.stringify({ data: 'Viacheslav Tyshchuk' }));
+    try {
+        switch (request.method) {
+            case 'GET':
+                if (isUrlEndsWithUsers) {
+                    output = getAllUsers();
+                }
+                else {
+                    const id = request.url ? getIdFromURL(request.url) : '';
+                    if (id) {
+                        output = getUserById(id);
+                    }
+                }
+                break;
+            default:
+                console.error(`${request.method} not working`);
+                break;
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+    response.statusCode = 200;
+    response.end(JSON.stringify(output));
 });
-const PORT = Number(process.env.PORT || 4000);
+const DEFAULT_PORT = 4000;
+const PORT = Number(process.env.PORT || DEFAULT_PORT);
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,82 @@
-import http from "node:http";
-import { User } from "./types/types";
+import { IncomingMessage, ServerResponse, createServer } from "node:http";
+// import { User } from "./types/types";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ data: 'Viacheslav Tyshchuk' }));
+interface  UserNoId  {
+  username: string;
+  age: number;
+  hobbies: string[];
+};
+
+interface UserWithId extends UserNoId {
+  id: string;
+};
+
+let users: UserWithId[] = [];
+
+
+
+const getAllUsers = () => users;
+
+
+const URL_BEFORE_ID_REGEXP:RegExp = /^\/api\/users\/$/;
+
+const getIdFromURL = (url: string):string => {
+  let id = url.replace(URL_BEFORE_ID_REGEXP, '');
+
+  if (id.endsWith('/')) {
+    id = id.slice(0, id.length - 1);
+  }
+
+  return id;
+};
+
+const getUserById = (id: string) => {
+  const userById = users.find(user => user.id === id);
+  return userById;
+};
+
+
+
+const server = createServer((request: IncomingMessage, response: ServerResponse<IncomingMessage>) => {
+  
+  const isUrlEndsWithUsers:boolean = 
+        (request.url === '/api/users') || (request.url === '/api/users/');
+  
+  let output: any;
+
+  response.setHeader("Content-Type", "application/json");
+  // response.statusCode = 200;
+  // response.end(JSON.stringify({ data: 'Viacheslav Tyshchuk' }));
+
+  try {
+    switch (request.method) {
+      case 'GET':
+        if (isUrlEndsWithUsers) {
+          output = getAllUsers();
+
+        } else {
+          const id = request.url? getIdFromURL(request.url): '';
+          if (id) { output = getUserById(id); }
+        }
+        break;
+    
+      default:
+        console.error(`${request.method} not working`);
+        break;
+    }
+    
+  } catch (error) {
+    console.error(error);
+  }
+  
+  response.statusCode = 200;
+  response.end(JSON.stringify(output));
+
 });
 
-const PORT = Number(process.env.PORT || 4000);
+
+const DEFAULT_PORT:number = 4000;
+const PORT = Number(process.env.PORT || DEFAULT_PORT);
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
